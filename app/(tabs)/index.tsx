bc4f916200b6da9b108fe1d2bd7e122d558b3684
ninako <ninako.wo.shi.25@gmail.com>
2025-05-29 20:46:36 +0900

    現在地を地図に表示

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,77 @@
-import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+// ホーム　地図
+
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Alert, StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function Home() {
+  const [location, setLocation] = useState<Location.LocationObjectCoords | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('位置情報の権限がありません');
+        return;
+      }
+
+      // リアルタイム位置取得
+      const subscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 3000,     // 3秒ごと
+          distanceInterval: 1,    // 1m動いたら更新
+        },
+        (loc) => {
+          setLocation(loc.coords);
+        }
+      );
+
+      // コンポーネントが消えたら位置監視停止
+      return () => {
+        subscription.remove();
+      };
+    })();
+  }, []);
+
+  if (!location) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 35.681236,
-          longitude: 139.767125,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: location.latitude,
+          longitude: location.longitude,
         }}
+        title="現在地"
+        description="リアルタイムに追従中"
       />
-    </View>
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
     flex: 1,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
